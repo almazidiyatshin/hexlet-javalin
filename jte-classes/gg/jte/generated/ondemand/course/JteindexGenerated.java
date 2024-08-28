b00eb1244af5fe35ce5f6f809fe7edd93469eb6b
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2024-08-28 19:35:31 +0300

    add jte layout

--- jte-classes/gg/jte/generated/ondemand/course/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/course/JteindexGenerated.java
@@ -2,9 +2,14 @@ package gg.jte.generated.ondemand.course;
 import org.example.hexlet.dto.courses.CoursesPage;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "course/index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,9,9,9,9,9,9,9,9,9,9};
+	public static final int[] JTE_LINE_INFO = {0,0,2,2,2,2,2,4,4,6,6,6,6,6,6,6,6};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
-		jteOutput.writeContent("\n<html>\n<head>\n    <title>Хекслет</title>\n</head>\n<body>\n<h1>OLOLOLO</h1>\n</body>\n</html>");
+		jteOutput.writeContent("\n");
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n<h1>OLOLOLO</h1>\n");
+			}
+		});
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
---
+++ jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
@@ -0,0 +1,16 @@
+package gg.jte.generated.ondemand.layout;
+import gg.jte.Content;
+public final class JtepageGenerated {
+	public static final String JTE_NAME = "layout/page.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,14,14,14,14,16,16,16,1,1,1,1};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, Content content) {
+		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"utf-8\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n    <title>Hexlet Javalin Example</title>\n</head>\n<body>\n<p>\n    <a href=\"/courses\">Courses</a>\n</p>\n");
+		jteOutput.setContext("body", null);
+		jteOutput.writeUserContent(content);
+		jteOutput.writeContent("\n</body>\n</html>");
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		Content content = (Content)params.get("content");
+		render(jteOutput, jteHtmlInterceptor, content);
+	}
+}
